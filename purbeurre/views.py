1c9608933c489e689023daedecf9302683e27b45
AntoineMaurin <antoine.maurin26@gmail.com>
2020-05-18 18:03:53 +0200

    Première version des templates statiques avec theme bootstrap

---
+++ purbeurre/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def homepage(request):
+    return render(request, "home.html")
+
+def accountpage(request):
+    return render(request, "my_account.html")
+
+def productpage(request):
+    return render(request, "product_page.html")
