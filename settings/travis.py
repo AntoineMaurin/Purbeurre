1e73e5777d2a4bcc6a067ee8c61d9c8b50e45785
AntoineMaurin <antoine.maurin26@gmail.com>
2020-07-06 19:48:27 +0200

    Mise à jour du fichier .travis et ajout d'un fichier de settings propre à travis

---
+++ settings/travis.py
@@ -0,0 +1,16 @@
+import os
+
+from purbeurre.settings import *
+
+SECRET_KEY = os.environ.get('SECRET_KEY', '8fsm7s-poju5ht*%j$@__r8(orf%o=nvei1l9q#0v$^1w+pu8=')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
+    }
+}
