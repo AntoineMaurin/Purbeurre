cfb15387e5d091ffd90ca0faed6d1518a1ab9fa9
AntoineMaurin <antoine.maurin26@gmail.com>
2020-07-06 18:07:36 +0200

    Mise à jour de la configuration pour travis

--- settings/production.py
+++ settings/production.py
@@ -1,28 +1,22 @@
-from dotenv import load_dotenv
-
-import os
-
 import django_heroku
 
 from purbeurre.settings import *
 
-load_dotenv()
-
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = "t-l@zgp9zyog3t%j5ee)1rqd600_(u9mz@b_8$bkohr@3d7@on"
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS']
+ALLOWED_HOSTS = "46.101.184.139"
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'NAME': 'purbeurre',
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DB_HOST'],
-        'PORT': os.environ['DB_PORT'],
+        'USER': 'user_purbeurre',
+        'PASSWORD': 'elpassword',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
