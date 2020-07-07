a1ce4b1e7f3d4374324c823c11f0c7ccce0153cc
AntoineMaurin <antoine.maurin26@gmail.com>
2020-07-07 18:10:38 +0200

    Màj du fichier travis sur les paramètres du chrome executable

--- purbeurre/tests/functionnal/test_chrome.py
+++ purbeurre/tests/functionnal/test_chrome.py
@@ -5,8 +5,13 @@ from purbeurre.models import Product, Category
 
 class PurbeurreChromeTest(StaticLiveServerTestCase):
     def setUp(self):
-        self.browser = webdriver.Chrome('purbeurre/tests/functionnal/'
-                                        'chromedriver')
+        try:
+            self.browser = webdriver.Chrome('purbeurre/tests/functionnal/'
+                                            'chromedriver_linux64')
+        except:
+            self.browser = webdriver.Chrome('purbeurre/tests/functionnal/'
+                                            'chromedriver')
+                                              
     def tearDown(self):
         self.browser.close()
 
