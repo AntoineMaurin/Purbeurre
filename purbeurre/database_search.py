5b5ed8e455e175a1ec914b2a0f5652cae01c1c75
Mathieu <mathieu@classnco.com>
2020-07-29 13:44:58 +0200

    Correct bug

--- purbeurre/database_search.py
+++ purbeurre/database_search.py
@@ -16,7 +16,7 @@ class DatabaseSearch:
         categories = self.get_categories_from_search(search)
 
         if categories is None:
-            return Product.objects.first()
+            return Product.objects.filter().first()
         else:
             for i, cat in enumerate(categories.keys()):
                 cat_subs_list.append({cat: []})
