f27370a43f2c151fb58e5141a8b963a31ff89ecf
AntoineMaurin <antoine.maurin26@gmail.com>
2020-08-03 15:43:17 +0200

    Mise à jour du test de la fonctionnalité pour trier les produits par nom avant d'en récupérer un.

--- purbeurre/tests/unit/test_database_search.py
+++ purbeurre/tests/unit/test_database_search.py
@@ -27,21 +27,22 @@ class DatabaseSearchTest(TestCase):
         for i in range(len(subs_per_category)):
             self.assertIn(supposed_categories[i], subs_per_category[i].keys())
 
-# test cassé par le bug
-    # def test_nonsense_search_gives_no_results(self):
-    #     result = self.dbs.get_substitutes_per_category('fsfklhsdflqj')
-    #     self.assertEquals(result, None)
+    def test_nonsense_search_gives_no_results(self):
+        result = self.dbs.get_substitutes_per_category('fsfklhsdflqj')
+        self.assertEquals(result, None)        
 
     def test_bad_products_are_not_given_in_results(self):
         good_nutriscores_list = ['a', 'b', 'c']
         products = []
 
         for category in Category.objects.all():
-            one_product = Product.objects.filter(category=category)[0]
+            one_product = Product.objects.filter(
+                                                 category=category
+                                                 ).order_by('name')[0]
             products.append(one_product)
 
         for product in products:
             category = self.dbs.get_substitutes_per_category(product.name)
 
-            for product in list(category[0].values())[0]:
-                self.assertIn(product.nutriscore, good_nutriscores_list)
+            for p in list(category[0].values())[0]:
+                self.assertIn(p.nutriscore, good_nutriscores_list)
